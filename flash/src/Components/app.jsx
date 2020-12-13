ff7482b54982b5f6909708ec9212ee3f2e3ff227
xxiong93 <56352660+xxiong93@users.noreply.github.com>
2020-12-12 18:17:17 -0600

    Installed axios, added folder and file for axios requests, set up app.jsx file with an app component,

---
+++ flash/src/Components/app.jsx
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+import axios from 'axios';
+
+class App extends Component {
+    state = {};
+
+
+
+
+
+    render() {
+        return(
+            <h1>TEST PAGE</h1>
+        )
+    }
+}
+
+export default App;
---
+++ flash/src/Components/axios/axios.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import Axios from 'axios';
+
+
+
+export default class FlashCardList extends React.Component {
+    state = {
+
+    }
+
+    componentDidMount(){
+        // Axios.get('http://localhost:5000/api/collections')
+        //     .then(response){
+        //         const data = response.data
+        //     }
+    }
+    
+    render() {
+        
+    }
+}
