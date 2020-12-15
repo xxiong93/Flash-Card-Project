b07b96b8ba25129ecd337fe00c513724792a1baf
xxiong93 <56352660+xxiong93@users.noreply.github.com>
2020-12-14 18:54:14 -0600

    Got react app to render successfully on browser.

--- flash/src/Components/app.jsx
+++ flash/src/Components/app.jsx
@@ -5,13 +5,9 @@ class App extends Component {
     state = {};
 
 
-
-
-
     render() {
-        return(
-            <h1>TEST PAGE</h1>
-        )
+        return(<h1>TEST PAGE</h1>,
+            <h2>Another one</h2>);
     }
 }
 
